2194110d7e114637a94f1e4be284198e33b70cd7
rohan <keez45264@gmail.com>
2026-09-11 02:22:36 +0530

    fix baking soda image path

---
+++ check_baking_soda.py
@@ -0,0 +1,8 @@
+﻿import psycopg2, os
+from dotenv import load_dotenv
+load_dotenv()
+conn = psycopg2.connect(os.environ.get('DATABASE_URL'))
+cur = conn.cursor()
+cur.execute("SELECT product_id, name, image_path FROM products WHERE name ILIKE '%baking soda%'")
+for row in cur.fetchall():
+    print(row)
